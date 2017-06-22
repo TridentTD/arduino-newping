0ad3710f3ce0078e50a6821c9e543498dbd2ee0b
TridentTD <prote2000@gmail.com>
2017-06-22 17:56:24 +0700

    test

--- src/mgos_arduino_newping.h
+++ src/mgos_arduino_newping.h
@@ -7,8 +7,8 @@ extern "C"
 {
 #endif
 
-NewPing  *mgos_newping_create(uint8_t trigger_pin, uint8_t echo_pin);
-void      mgos_newping_close(NewPing *newping);
+NewPing       *mgos_newping_create(uint8_t trigger_pin, uint8_t echo_pin);
+void           mgos_newping_close(NewPing *newping);
 unsigned long  mgos_newping_ping_cm(NewPing *newping);
 
 #ifdef __cplusplus
